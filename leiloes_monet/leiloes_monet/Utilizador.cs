396e452ea049bc7a723a8367bf315edeb0e9817d
HenriqueVaz <henriquevaz7@hotmail.com>
2023-11-14 00:44:20 +0000

    added razor project

---
+++ leiloes_monet/leiloes_monet/Utilizador.cs
@@ -0,0 +1,15 @@
+﻿namespace leiloes_monet.Pages
+{
+    [Serializable]
+    public class Utilizador
+    {
+        public String email { get; set; }
+        public String password { get; set; }
+        public String nome { get; set; }
+        public DateOnly data_nascimento { get; set; }
+        public String nif { get; set; }
+        //public Morada morada { get; set; }
+
+    }
+
+}
