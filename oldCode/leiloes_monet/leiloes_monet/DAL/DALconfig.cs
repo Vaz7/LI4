23b57a1ca3eade3dca931bb26741b61367b09836
Henrique Vaz <henriquevaz7@hotmail.com>
2024-01-09 20:43:30 +0000

    old code

---
+++ oldCode/leiloes_monet/leiloes_monet/DAL/DALconfig.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace leiloes_monet.DAL
+{
+    public class DALconfig
+    {
+        public static string connectionstring = @"Data Source=VAZ;Initial Catalog=Li4;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+    }
+}
